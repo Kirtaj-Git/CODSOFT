4e4e15396759f4d20e8e2733d8d312ba3bce08ef
Kirtaj-Git <152432209+Kirtaj-Git@users.noreply.github.com>
2023-11-30 00:04:56 +0530

    Add files via upload

---
+++ iop.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+ class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+public class iop {
+    public static void main(String[] args) {
+        BankAccount userAccount = new BankAccount(1000.0);
+        Scanner scanner = new Scanner(System.in);
+
+        int choice = 0;
+        while (choice != 4) {
+            System.out.println("1. Withdraw");
+            System.out.println("2. Deposit");
+            System.out.println("3. Check Balance");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter amount to withdraw: ");
+                    double withdrawAmount = scanner.nextDouble();
+                    if (userAccount.withdraw(withdrawAmount)) {
+                        System.out.println("Successfully withdrew $" + withdrawAmount);
+                    } else {
+                        System.out.println("Insufficient funds");
+                    }
+                    break;
+                case 2:
+                    System.out.print("Enter amount to deposit: ");
+                    double depositAmount = scanner.nextDouble();
+                    userAccount.deposit(depositAmount);
+                    System.out.println("Successfully deposited $" + depositAmount);
+                    break;
+                case 3:
+                    System.out.println("Your balance is $" + userAccount.getBalance());
+                    break;
+                case 4:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+                    break;
+            }
+        }
+        scanner.close();
+    }
+}
